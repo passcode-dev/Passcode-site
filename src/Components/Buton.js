3265b257d078cf8b44d54124bef07d8a1e170791
Felipe Huss Mendes Pereira <felipehuss03@gmail.com>
2025-03-04 15:13:17 -0300

    feat:Header

---
+++ src/Components/Buton.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+export const Button=styled.button`
+    background-color: #3d63ea;
+    padding-top: 19px;
+    padding-right: 30px;
+    padding-bottom: 19px;
+    padding-left: 30px;
+    gap: 10px;
+    border-radius: 5px;
+    color: white;
+    border: none;
+    box-shadow: 0px 4px 49px 0px #00000026;
+`
