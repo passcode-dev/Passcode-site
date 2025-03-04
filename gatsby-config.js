0775e20b289ea6ce851f11e5a37cec357c1d7594
Felipe Huss Mendes Pereira <felipehuss03@gmail.com>
2025-03-04 11:30:04 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `PassCode`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
