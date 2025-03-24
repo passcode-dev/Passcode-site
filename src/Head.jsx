1e5fbf81f6f6961b20f70547bea8fa6a61fa1862
luanhirai <luanhiraione@outlook.com>
2025-03-24 17:05:05 -0300

    hotfix: mudança no head

--- src/Head.jsx
+++ src/Head.jsx
@@ -5,8 +5,14 @@ export function Head() {
         <>
             <html lang="pt-br" />
 
-            <title>Passcode | Oficial</title>
-            <meta name="description" content="Passcode" />
+            <title>Passcode - Desenvolvimento de Software</title>
+            <meta name="description" content="Passcode é uma software house especializada em desenvolvimento de aplicativos móveis, design de sites, testes de software e soluções em inteligência artificial." />
+            <meta name="keywords" content="software house, desenvolvimento de aplicativos móveis, design de sites, testes de software, inteligência artificial, back-end, soluções em nuvem, qualidade de software" />
+            <meta name="author" content="Passcode" />
+            <meta property="og:title" content="Passcode - Desenvolvimento de Software" />
+            <meta property="og:description" content="Transforme suas ideias em soluções digitais de alta performance. Desenvolvimento de apps, sites e soluções em IA." />
+            <meta name="description" content="Passcode é uma software house em São Paulo, especializada em desenvolvimento de aplicativos móveis, design de sites, testes de software e soluções em inteligência artificial." />
+            <meta name="keywords" content="software house São Paulo, desenvolvimento de aplicativos móveis, design de sites, testes de software, inteligência artificial, back-end, soluções em nuvem" />
         </>
     )
 }
